3e3e9d4f555819a9494a1f4e987ae07295ec199c
Bidhun <bidhunvaikkath@gmail.com>
2024-09-02 19:04:31 +0530

    Implement quiz complete screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,16 +1,27 @@
 import { useState } from "react";
 import QUESTIONS from "../Question";
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() =>Math.random() - 0.5);
+  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;  
 
   function handleSelectAnswer(selectedAnswer){
     setUserAnswers((prevUserState) => [...prevUserState , selectedAnswer]);
   }
+
+  if(quizIsCompleted){
+    return <div id="summary">
+      <img src={quizCompleteImg} alt="Trophy Icon" />
+      <h2>Quiz completed!</h2>
+    </div>
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() =>Math.random() - 0.5);
+
   return (
     <div id="quiz">
          <div id="question">
