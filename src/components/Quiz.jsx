8382158b8c07195d23bfddf85ebe24fdb3398f28
Bidhun <bidhunvaikkath@gmail.com>
2024-09-03 21:44:47 +0530

    fix the issues in progress bar by using useEffect and callback

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,40 +1,59 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from "../Question";
-import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from "./QuestionTimer.jsx";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
-  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;  
+  const quizIsCompleted = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer){
-    setUserAnswers((prevUserState) => [...prevUserState , selectedAnswer]);
-  }
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
 
-  if(quizIsCompleted){
-    return <div id="summary">
-      <img src={quizCompleteImg} alt="Trophy Icon" />
-      <h2>Quiz completed!</h2>
-    </div>
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsCompleted) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy Icon" />
+        <h2>Quiz completed!</h2>
+      </div>
+    );
   }
 
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() =>Math.random() - 0.5);
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
-         <div id="question">
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id="answers">
-        {shuffledAnswers.map((answer) => (
-          <li key={answer} className="answer">
-            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
+      <div id="question">
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        />
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    </div>
-   
   );
 }
